998f791e39f0ae649fb0830ab1a096ee3f25ba2f
santosh <santosh.ece06@gmail.com>
2017-05-16 11:03:39 +0530

    commit

--- assets/main.js
+++ assets/main.js
@@ -1,48 +1,46 @@
 var app = angular.module("app", []);
 
-app.controller("first", function($scope) {
-    var mypost = {
-        "post": "Hello CDAC, Welcome to Khargar",
-        "like": 50,
-        "dislike": 100,
-        "address": {
-            "city": "MUMBAI"
-        }
+app.controller("first", function($scope, $http) {
+    $scope.appName = "CDAC";
+
+    $scope.newPost = {
+        "POST_TYPE": "TEXT",
+        "PATH_OF_POST": null
     };
 
-    var username = "CDAC MUMBAI";
-
-    $scope.mypost = mypost;
-    $scope.username = username;
-
-    $scope.postList = [{
-            "post": "Hello CDAC, Welcome to Khargar",
-            "like": 50,
-            "dislike": 100
-        },
-        {
-            "post": "Hello CDAC, Welcome to Khargar",
-            "like": 50,
-            "dislike": 100
-        },
-        {
-            "post": "Hello CDAC, Welcome to Khargar",
-            "like": 50,
-            "dislike": 100
-        },
-        {
-            "post": "Hello CDAC, Welcome to Khargar",
-            "like": 50,
-            "dislike": 100
-        }, {
-            "post": "Hello CDAC, Welcome to Khargar",
-            "like": 50,
-            "dislike": 100
-        }
-    ];
-});
+    var postList = [];
+    $scope.postList = postList;
 
 
-app.controller("second", function($scope) {
-    $scope.movie = { "name": "Dr. Strange" };
+
+    $scope.createPost = function() {
+        console.log($scope.newPost);
+
+        var url = "http://localhost:3010/post/";
+        var hpromise = $http.post(url, $scope.newPost);
+        hpromise.then(function(response) {
+            console.log(response);
+
+            $scope.readAllPost();
+            $scope.newPost.POST = "";
+        }).catch(function(err) {
+            console.log(err);
+        });
+    };
+
+
+    $scope.readAllPost = function() {
+        var url = "http://localhost:3010/post/";
+        var hpromise = $http.get(url);
+        hpromise.then(function(response) {
+            console.log(response.data);
+
+            $scope.postList = response.data;
+
+        }).catch(function(err) {
+            console.log(err);
+        });
+    };
+    $scope.readAllPost();
+
 });
